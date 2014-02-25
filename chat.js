4b5783d74f6b35b42c80236c57727fd671e678a3
Louis Angelini <lllouis@yahoo.com>
2014-02-25 14:59:45 +0000

    Initial commit. Basic chat server, retrieving old messages not working yet

---
+++ chat.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var redis = require('redis');
+var redisClient = redis.createClient();
+var app = express();
+var http = require('http');
+app.get('/',function(request,response){
+	response.sendfile(__dirname + '/index.html');
+});
+var server = http.createServer(app);
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(client){
+	console.log('Client connected');
+	client.on('join', function(name){
+		client.set('nickname', name);
+		redisClient.lrange("messages", 0,49, function(err, messages){
+			messages = messages.reverse();
+
+			messages.forEach(function(message){
+				message = JSON.parse(message);
+				client.emit('chat', message.name + ": " +message.data);
+			});
+		});
+	});
+	client.on('messages',function(data){
+		client.get('nickname', function(err,name){
+			storeMessage(name, data);
+			client.broadcast.emit("chat", {name:name, message:data});
+		});
+		
+	});
+});
+
+var storeMessage = function(data, name){
+	var message = JSON.stringify({ name: name, data: data });
+	redisClient.lpush("messages", message, function(err, response){
+		redisClient.ltrim("messages", 0,10);
+	});
+}
+
+server.listen(1337);
